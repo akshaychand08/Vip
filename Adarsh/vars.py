dd78cd4222c3f54414fd3be3517e51d6afbaaf84
Nomanmalik001 <131957268+Nomanmalik001@users.noreply.github.com>
2023-05-02 23:42:21 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -16,20 +16,20 @@ load_dotenv()
 
 class Var(object):
     MULTI_CLIENT = False
-    API_ID = int(getenv('API_ID', ""))
-    API_HASH = str(getenv('API_HASH', ""))
-    BOT_TOKEN = str(getenv('BOT_TOKEN', ""))
-    name = str(getenv('name', 'testbot'))
+    API_ID = int(getenv('API_ID', '19455849'))
+    API_HASH = str(getenv('API_HASH', '71498d69fad014add7e9f717bfde4b79'))
+    BOT_TOKEN = str(getenv('BOT_TOKEN', '5451383761:AAGYe32Dv1Qe2NBvogzclNzCx-7Geup9pYc'))
+    name = str(getenv('name', 'Bottest'))
     SLEEP_THRESHOLD = int(getenv('SLEEP_THRESHOLD', '60'))
     WORKERS = int(getenv('WORKERS', '4'))
-    BIN_CHANNEL = int(getenv('BIN_CHANNEL', "-"))
+    BIN_CHANNEL = int(getenv('BIN_CHANNEL', '-1001620024364'))
     PORT = int(getenv('PORT', 8080))
     BIND_ADRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
     OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "905710386").split())  
     NO_PORT = bool(getenv('NO_PORT', False))
-    APP_NAME = str(getenv('APP_NAME'))
-    OWNER_USERNAME = str(getenv('OWNER_USERNAME'))
+    APP_NAME = None
+    OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'sahid_malik'))
     if 'DYNO' in environ:
         ON_HEROKU = True
         APP_NAME = str(getenv('APP_NAME'))
@@ -37,16 +37,17 @@ class Var(object):
     else:
         ON_HEROKU = False
     FQDN = str(getenv('FQDN', BIND_ADRESS)) if not ON_HEROKU or getenv('FQDN') else APP_NAME+'.herokuapp.com'
-    FQDN = "streamtesting.herokuapp.com" 
+    FQDN = "malikbots.onrender.com" 
     HAS_SSL = True
     HAS_SSL=bool(getenv('HAS_SSL',False))
     if HAS_SSL:
         URL = "https://{}/".format(FQDN)
     else:
-        URL = "https://{}/".format(FQDN)
-    DATABASE_URL = str(getenv('DATABASE_URL', ""))
+        URL = "http://{}/".format(FQDN)
+    DATABASE_URL = str(getenv('DATABASE_URL', 'mongodb+srv://savez:malik@cluster0.vsnui.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
     BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "-1001362659779")).split())) 
+
 
 
     BOT_USERNAME = str(getenv('BOT_USERNAME'))
@@ -56,7 +57,7 @@ class Var(object):
     AD4 = str(getenv('AD4', ""))
     AD5 = str(getenv('AD5', ""))
     AD6 = str(getenv('AD6', ""))
-    USERS_CAN_USE = getenv('USERS_CAN_USE', False)
+    USERS_CAN_USE = getenv('USERS_CAN_USE', True)
     ADMINS = (
         [int(i.strip()) for i in os.environ.get("ADMINS").split(",")]
         if os.environ.get("ADMINS")
