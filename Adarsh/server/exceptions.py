b859b95c8c63a185f45f092897b1a87795504499
Snehal <96618953+Snehal-k10@users.noreply.github.com>
2023-11-07 22:14:05 +0530

    Update exceptions.py

--- Adarsh/server/exceptions.py
+++ Adarsh/server/exceptions.py
@@ -2,4 +2,4 @@ class InvalidHash(Exception):
     message = "Invalid hash"
 
 class FIleNotFound(Exception):
-    message = "File not found"
+    message = "File not found. The files have been deleted due to some technical reason. please Contact @admin to upload new Files"
